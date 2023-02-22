1a6f80aa1e9f662272991995999a17eb28e9a481
sirq <77940766+Sir-Quack-III@users.noreply.github.com>
2023-02-21 20:38:25 -0800

    ruined everything

--- src/forrus.cpp
+++ src/forrus.cpp
@@ -1,20 +1,63 @@
 #include "forrus.hpp"
 
+unsigned char leftRotate(unsigned char val, int n) 
+{ 
+  unsigned int t; 
+ 
+  t = val; 
+ 
+  for(int i=0; i < n; i++) { 
+    t = t << 1; 
+ 
+    /* If a bit shifts out, it will be in bit 8 
+       of the integer t. If this is the case, 
+       put that bit on the right side. */ 
+    if(t & 256) 
+      t = t | 1; // put a 1 on the right end 
+  } 
+ 
+  return t; // return the lower 8 bits. 
+} 
+
+int myPow(int x, unsigned int p)
+{
+  if (p == 0) return 1;
+  if (p == 1) return x;
+  
+  int tmp = myPow(x, p/2);
+  if (p%2 == 0) return tmp * tmp;
+  else return x * tmp * tmp;
+}
+
+
+// uint8_t leftRotate(uint8_t n, uint8_t d) {
+//     return (n << d)|(n >> (8 - d));
+// }
+
 uint8_t forrus::round(uint8_t inp, uint8_t key) {
     uint8_t out = inp;
 
-    
+    out ^= key;
+    out = leftRotate(out, 3);
+    out = (uint8_t)((int)pow((float)out, 5.0) % 173);
+    out ^= (uint8_t)((int)pow((float)key, 5.0) % 173);
+
+    return out;
 }
 std::array<uint8_t, 16> forrus::hash(std::array<uint8_t, 16> inp) {
     std::array<uint8_t, 16> hash_in = inp;
-    std::array<uint8_t, 16> hash_out;
-
-    size_t idx;
+    std::array<uint8_t, 16> hash_out = {0};
 
     for (size_t idx = 0; idx < 16; idx++) {
         uint8_t i = hash_in[idx];
+        uint8_t c = round(i, 2);
 
+        for (int d = 0; d < 14; d++) {
+            c = round(c, 2);
+        }
+
+        hash_out[idx] = round(hash_in[idx], c);
     }
 
-    return {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    return hash_out;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "forrus.hpp"
 
 int main() {
-    std::cout << "balls" << std::endl;
+    std::array<uint8_t, 16> inp = {2, 90, 126, 43, 183, 111, 40, 193, 205, 23, 153, 64, 93, 42, 82, 69};
+    std::array<uint8_t, 16> out = forrus::hash(inp);
+    
+    for (int i = 0; i < inp.size(); i++) printf("%d, ", inp[i]);
+    std::cout << std::endl;
+
+    for (int i = 0; i < out.size(); i++) printf("%d, ", out[i]);
+    std::cout << std::endl;
+
+    printf("%d\n", forrus::round(128, 4));
     return 0;
 }
